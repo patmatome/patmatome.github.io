7c2246ed74e2640455f8dffc186754855921dfb4
Matome Pat Maela <61184959+patmatome@users.noreply.github.com>
2024-05-23 10:53:40 +0200

    Update drift.js

--- drift.js
+++ drift.js
@@ -4,18 +4,29 @@
 var userNameInput =document.getElementById('name')
 var userEmailInput = document.getElementById('user-email')
 var userMsg = document.getElementById('to-user-input');
+var form = document.getElementById('form');
+var enquery = document.getElementById('enqueryMessege');
 
 /* taking user input*/
+form.addEventListener("submit",(event) =>{
+    event.preventDefault()
+});
 function sendEmail(){
     try {
         if(userNameInput.value==""||userEmailInput.value==""||userNameInput.value==null){
 
-            userMsg.style="color:red"        
-            userMsg.innerHTML = "PLEASE ENTER ALL REQUIRED DETAILS"
+            userMsg.style="color:red; font-style:italic;"        
+            userMsg.innerHTML = "PLEASE ENTER ALL REQUIRED DETAILS!!!"
         }
-        else{
+        else if(enquery.value.length<=3){
+            userMsg.style="color:yellow; font-style:italic;"     
             var name = userNameInput.value
-            alert("THANK YOU "+name.toUpperCase()+", YOU'R MESSEGE HAS BEEN SUCCCEFULLY SENT I'LL BE IN TOUCH")    
+            userMsg.innerHTML=name.toUpperCase()+" your message must have atleast two word!!"  
+            }
+        else{
+            userMsg.style="color:green; font-style:italic;"     
+            var name = userNameInput.value
+            userMsg.innerHTML="THANK YOU "+name.toUpperCase()+", YOU'R MESSEGE HAS BEEN SUCCCEFULLY SENT I'LL BE IN TOUCH..."  
         }
         
     } catch (error) {
