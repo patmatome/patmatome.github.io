7ecbccbe23f76333fc76463d9e3edcbf11d94370
Matome Pat Maela <61184959+patmatome@users.noreply.github.com>
2024-05-23 18:29:25 +0200

    Update drift.js

--- drift.js
+++ drift.js
@@ -5,7 +5,7 @@ var userNameInput =document.getElementById('name')
 var userEmailInput = document.getElementById('user-email')
 var userMsg = document.getElementById('to-user-input');
 var form = document.getElementById('form');
-var enquery = document.getElementById('enqueryMessege');
+var message = document.getElementById('enqueryMessege');
 
 /* taking user input*/
 form.addEventListener("submit",(event) =>{
@@ -18,16 +18,21 @@ function sendEmail(){
             userMsg.style="color:red; font-style:italic;"        
             userMsg.innerHTML = "PLEASE ENTER ALL REQUIRED DETAILS!!!"
         }
-        else if(enquery.value.length<=3){
+        else if(message.value.length<=3){
             userMsg.style="color:yellow; font-style:italic;"     
             var name = userNameInput.value
             userMsg.innerHTML=name.toUpperCase()+" your message must have atleast two word!!"  
             }
         else{
+            let params={
+                name,
+                email,
+                message
+            }
             userMsg.style="color:green; font-style:italic;"     
             var name = userNameInput.value
             userMsg.innerHTML="THANK YOU "+name.toUpperCase()+", YOU'R MESSEGE HAS BEEN SUCCCEFULLY SENT I'LL BE IN TOUCH..."  
-            emails.send("service_vtzxchl", "template_qlxz9e4");
+            emails.send("service_vtzxchl", "template_qlxz9e4", params);
         }
         
     } catch (error) {
