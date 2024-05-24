04cb5b3b5729ef4c6cffd117c84051c3b6c7eff8
Matome Pat Maela <61184959+patmatome@users.noreply.github.com>
2024-05-24 16:39:07 +0200

    Update drift.js

--- drift.js
+++ drift.js
@@ -2,16 +2,18 @@
 /** importing html elements to my javascript txt file for functionality and
  * modification
 */
-
-function sendEmail(){
-           let params={
+let params={
                 name:document.getElementById('name').value,
                 email:document.getElementById('user-email').value,
                 message:document.getElementById('enqueryMessege').value
 
             }
+
+function sendEmail(){
+           
             emailjs.send("service_vtzxchl", "template_qlxz9e4", params).then((response)=> {
       console.log('SUCCESS!');
+       alert("THANK YOU "+name.toUpperCase()+", YOU'R MESSEGE HAS BEEN SUCCCEFULLY SENT I'LL BE IN TOUCH..." )               
     }, function(error) {
       console.log('FAILED...', error);
     });
