c3ac7a07c4c95e26830c74eb53c1d3c1a2ad9136
Matome Pat Maela <61184959+patmatome@users.noreply.github.com>
2024-06-25 14:35:32 +0200

    Update let_x=0;.js

--- let_x=0;.js
+++ let_x=0;.js
@@ -2,8 +2,20 @@ const jet = document.querySelector('#jet')
 const btnBet1 = document.getElementById('btn-bet1')
 let muliplier = document.querySelector('#zone-value')
 let btnBet2 =  document.getElementById('btn2')
-const autoBet = document.getElementById('check1')
-let autoBet2 = document.getElementById('check2')
+let autoBet = document.getElementById('autoBet')
+let autoBet2 = document.getElementById('autoBet2')
+let aviBackground = document.querySelector('.avi-background')
+let cZone2 = document.getElementById('cZone2')
+let cZone = document.getElementById('cZone')
+let autoCashout = document.getElementById('autoCashout')
+let autoCashout2 = document.getElementById('autoCashout2')
+let toast = document.getElementById('toastCash')
+let C = document.getElementById('C')
+let toast2 = document.getElementById('toastCash2')
+let toastlvl = document.getElementById('toastLvl')
+let toastlvl2 = document.getElementById('toastlvl2')
+let C2 = document.getElementById('C2')
+console.log(autoCashout2)
 
 /*DEALING WITH THE FINANCECE*/
 let TotalBalance = document.querySelector('#balance-value')
@@ -42,16 +54,29 @@ function genarate() {
 function fly() {
 
     setInterval(() => {
+
         increminateMultiplier() 
-
-        jet.style = "background-color:purple"
-
+        
     genarate()
     
     //console.log(genarate())
     
     //console.log(animeDuration)
-
+    if(autoBet.checked&cZone.value>=1.1){
+        btnBet1.style="background-color:red"
+           btnBet1.innerText="Waiting For Next round"
+           ///btnBet1.innerText += "\n"+parseFloat(BetAmount.value)
+           BetAmount.style="background-color:orange"
+           TotalBalance.innerText = eval(parseFloat(TotalBalance.innerText-BetAmount.value))
+   }
+   console.warn("c")
+   if(autoBet2.checked&cZone2.value>=1.1){
+    btnBet2.style="background-color:red"
+       btnBet2.innerText="Waiting For Next round"
+       ///btnBet1.innerText += "\n"+parseFloat(BetAmount.value)
+       BetAmount2.style="background-color:orange"
+       TotalBalance.innerText = eval(parseFloat(TotalBalance.innerText-BetAmount2.value))
+}
  
     //console.warn(interDuration)
        // console.log(animeDuration)
@@ -74,13 +99,18 @@ function increminateMultiplier() {
 
             
         }
-        
+        backgroundAnimation.play()
         var targetzone = parseFloat(1+animeDuration/1000)
         var currentZone = parseFloat(sec+"."+millSec)
 
         if(currentZone==targetzone||currentZone>targetzone){
-            jet.style = "background-color:green"
+            //jet.style = "background-color:green"
+            C.style = "display: none"
+            ///toast.innerHTML=""
+            C2.style = "display: none"
+
             clearInterval(counter)
+            aviBackground.style = "transform:rotate(0)"
             setTimeout(() => {
                  sec=1
                 millSec=0
@@ -92,10 +122,12 @@ function increminateMultiplier() {
                 let bStatus= btnBet1.innerText.slice(0,7)
                 if(bStatus==="CashOut"){
                     Restart()
-                }else{
+                }else if (autoBet.checked){
                    bet()
                 }
                 btnCtr=1
+                backgroundAnimation.pause()
+
                 let bStatus2= btnBet2.innerText.slice(0,7)
 
                 if(bStatus2==="CashOut"){
@@ -106,30 +138,44 @@ function increminateMultiplier() {
                
                 btnCtr2=1
                 
-                
             }, 10);
             
-            
+          
         }
        
-        jet.style = "background-color:orange"
+        //jet.style = "background-color:orange"
         btnCtr=null         
         btnCtr2=null         
         //Betting Button
+
         if(btnBet1.innerHTML=="Waiting For Next round"&BetAmount.value<=TotalBalance.innerText&&currentZone<=1.1){
             control=null
-
+            
             var addBalace = setInterval(() => {
             let CurrentBalace = TotalBalance.innerText
             let targetPrice = parseFloat(1+animeDuration/1000)*BetAmount.value
-            let currentPrice = currentZone*BetAmount.value
+            let CashoutAim = parseFloat(sec+"."+millSec)
+            let stopAim = eval(parseFloat(cZone.value)+0.1)
             
+            if(CashoutAim==stopAim&autoCashout.checked){
+                console.log("Here..."+CashoutAim)
+                setTimeout(() => {
+                    C2.style = "display: block"
+                    toastlvl2.innerHTML = "Cashout at x "+sec+"."+millSec
+                    toast2.innerHTML="R "+eval(parseFloat(BetAmount.value*parseFloat(sec+"."+millSec)))
+                }, 1);
+
+                control=1
+                bet()
+                TotalBalance.innerHTML= eval(parseFloat(TotalBalance.innerHTML)+(cZone.value*BetAmount.value))
+                clearInterval(addBalace)
+            }
+           
+            //SETTING THE BTN TO CORRESPOND WITH THE FLEW AWAY STATE
             //SETTING THE BTN TO CORRESPOND WITH THE FLEW AWAY STATE
             if(btnCtr==null){
-                btnBet1.innerHTML="CashOut"+"<br>"+parseFloat(sec+"."+millSec)*BetAmount.value
+                btnBet1.innerHTML="CashOut"+"<br>"+parseFloat(sec+"."+millSec)*BetAmount2.value
                 fBtnBet()
-                console.warn(animeDuration-1)
-
 
             }
             else if(btnCtr==1){
@@ -152,6 +198,12 @@ function increminateMultiplier() {
                 let SM =parseFloat(sec+"."+millSec)
                 if(control==null&btnCtr==null){
                         TotalBalance.innerText=eval(parseFloat(SM)*parseFloat(BetAmount.value)+parseFloat(CurrentBalace))
+                        setTimeout(() => {
+                            C2.style = "display: block"
+                            toastlvl2.innerHTML = "Cashout at x "+sec+"."+millSec
+                            toast2.innerHTML="R "+eval(parseFloat(BetAmount.value*parseFloat(sec+"."+millSec)))
+                        }, 1);
+
                         control=1
                         Restart()
                     }
@@ -166,15 +218,31 @@ function increminateMultiplier() {
            // clearInterval(addBalace)
            // Restart()
         }
+       
         if(btnBet2.innerHTML=="Waiting For Next round"&BetAmount2.value<=TotalBalance.innerText&&currentZone<=1.1){
             
-            control=null
+            control2=null
 
             var addBalace2 = setInterval(() => {
             let CurrentBalace2 = TotalBalance.innerText
             let targetPrice2 = targetzone*BetAmount2.value
             let currentPrice2 = currentZone*BetAmount2.value
-            
+            let CashoutAim2 = parseFloat(sec+"."+millSec)
+            let stopAim2 = eval(parseFloat(cZone2.value)+0.1)
+           
+            if(CashoutAim2==stopAim2&autoCashout2.checked){
+                console.log("Here..."+CashoutAim2)
+                 setTimeout(() => {
+                            C.style = "display: block"
+                            toastlvl.innerHTML = "Cashout at x "+sec+"."+millSec
+                            toast.innerHTML="R "+eval(parseFloat(BetAmount.value*parseFloat(sec+"."+millSec)))
+                            
+                        }, 10);
+                        control2=1
+                bet()
+                TotalBalance.innerHTML= eval(parseFloat(TotalBalance.innerHTML)+(cZone2.value*BetAmount2.value))
+                clearInterval(addBalace2)
+            }
             //SETTING THE BTN TO CORRESPOND WITH THE FLEW AWAY STATE
             if(btnCtr2==null){
                 btnBet2.innerHTML="CashOut"+"<br>"+parseFloat(sec+"."+millSec)*BetAmount2.value
@@ -182,12 +250,12 @@ function increminateMultiplier() {
 
             }else if(btnCtr2==1){
                 //btnBet1.innerHTML= "BET"
-                //Restart2()
+                Restart2()
                 clearInterval(addBalace2)
 
             }
             let betStatus = btnBet2.innerHTML.slice(0, 7)
-           
+            
             //console.log('TotalBalance.innerText', TotalBalance.innerText)
 
             //CASHOUT BUTTON
@@ -197,6 +265,12 @@ function increminateMultiplier() {
                 let SM2 =parseFloat(sec+"."+millSec)
                 if(control2==null&btnCtr==null){
                         TotalBalance.innerText=eval(parseFloat(SM2)*parseFloat(BetAmount2.value)+parseFloat(CurrentBalace2))
+                        setTimeout(() => {
+                            C.style = "display: block"
+                            toastlvl.innerHTML = "Cashout at x "+sec+"."+millSec
+                            toast.innerHTML="R "+eval(parseFloat(BetAmount.value*parseFloat(sec+"."+millSec)))
+                            
+                        }, 10);
                         control2=1
                         Restart2()
                     }
@@ -220,6 +294,7 @@ function increminateMultiplier() {
 
     aviAnimate() 
 
+
     }
     
     , interDuration); 
@@ -233,25 +308,25 @@ function aviAnimate() {
             
         },
      {
-            transform: "translate(8vw,-4vw)",
+            transform: "translate(8%,-4%)",
     
         },{
-            transform: "translate(16vw,-8vw)"
+            transform: "translate(16%,-8%)"
     
         }, 
       {
-            transform: "translate(32vw,-16vw)"
+            transform: "translate(32%,-16%)"
     
         }, 
     {
-            transform: "translate(64vw,-32vw)"
+            transform: "translate(64%,-32%)"
     
         },{
-            transform:"translate(64vw,-30vw)",
+            transform:"translate(64%,-32%)",
             
         },
         {
-            transform:"translate(128vw,-64vw)",
+            transform:"translate(128vw,-30vw)",
             
         }
     ], {
@@ -262,18 +337,22 @@ function aviAnimate() {
     
 }
 
+
  increminateMultiplier() 
  bet()
  Restart()
 
 function bet() {
+   
+    //jet.style = "background-color:purple"
+console.warn("jj")
     btnBet1.addEventListener('click',()=>{
         if(btnBet1.innerHTML=="BET"){
             btnBet1.style="background-color:red"
             btnBet1.innerText="Waiting For Next round"
             ///btnBet1.innerText += "\n"+parseFloat(BetAmount.value)
-            BetAmount.style="background-color:orange"
-            TotalBalance.innerText = parseFloat(TotalBalance.innerText-BetAmount.value)
+            //BetAmount.style="background-color:orange"
+           TotalBalance.innerText = parseFloat(TotalBalance.innerText-BetAmount.value)
             
         }if(BetAmount.value<1||BetAmount.value>200){
             alert("Bet not placed, Amount must be between R1 -R200")
@@ -285,19 +364,20 @@ function bet() {
  
         //console.log(TotalBalance)
     });
+    
     btnBet2.addEventListener('click',()=>{
         if(btnBet2.innerHTML=="BET"){
             btnBet2.style="background-color:red"
             btnBet2.innerText="Waiting For Next round"
             ///btnBet1.innerText += "\n"+parseFloat(BetAmount.value)
-            BetAmount2.style="background-color:orange"
-                TotalBalance.innerText = parseFloat(TotalBalance.innerText-BetAmount2.value)
+            //BetAmount2.style="background-color:orange"
+            TotalBalance.innerText = parseFloat(TotalBalance.innerText-BetAmount2.value)
             
         }if(BetAmount2.value<1||BetAmount2.value>200){
             alert("Bet not placed, Amount must be between R1 -R200")
             Restart2()
         }
-         else if (btnBet2.innerHTML=="Waiting For Next round"&BetAmount2.value>TotalBalance.innerText){
+         else if (btnBet2.innerText=="Waiting For Next round"&BetAmount2.value>TotalBalance.innerText){
                Restart2()
 
             }
@@ -329,4 +409,19 @@ function fBtnBet() {
 function fBtnBet2() {
     btnBet2.style = "background-color: orange"
 }        
+
+   let backgroundAnimation = aviBackground.animate([ {
+        transform: "rotate(0)"
+    },
+    {
+        transform: "rotate(360deg)"
+    }],{
+        duration:10000,
+        easing:"linear",
+        iterations: 1
+    });
+   
+    backgroundAnimation.pause()
+            
+            
             
