51532951bc4245666dc598dbc61d07793ebbef24
Matome Pat Maela <61184959+patmatome@users.noreply.github.com>
2024-11-03 02:32:37 +0200

    Add files via upload

---
+++ milkyway.js
@@ -0,0 +1,11 @@
+
+    const nav = document.querySelector('.topHeader')
+    const h = document.getElementById('header')
+window.addEventListener('scroll',function (){
+    nav.style = "background-color:black; position:fixed; top:0"
+
+   h.style = "margin-top: 21vh;"
+
+
+}
+)
