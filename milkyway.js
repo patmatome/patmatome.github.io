c2483f2d2c1449289a223377232699511566a445
Matome Pat Maela <61184959+patmatome@users.noreply.github.com>
2024-12-02 02:48:33 +0200

    Update milkyway.js

--- milkyway.js
+++ milkyway.js
@@ -2,7 +2,7 @@
     const nav = document.querySelector('.topHeader')
     const h = document.getElementById('header')
 window.addEventListener('scroll',function (){
-    nav.style = "background-color:black; position:fixed; top:0"
+    nav.style = "background-color:black; position:fixed; top:0px; z-index:999999999;"
 
    h.style = "margin-top: 21vh;"
 
