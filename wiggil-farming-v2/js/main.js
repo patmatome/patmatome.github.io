029b928468323a1a712addd9bdcc53fef4dba95b
Matome Pat Maela <61184959+patmatome@users.noreply.github.com>
2025-12-11 18:58:48 +0200

    Add files via upload

---
+++ wiggil-farming-v2/js/main.js
@@ -0,0 +1,6 @@
+
+// TODO: Initialize EmailJS for contact forms
+// Example:
+// emailjs.init("YOUR_PUBLIC_KEY");
+// emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams);
+let num = 1
